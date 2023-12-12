4e0aca2e9e47849acdc8917c651925c3912a7b25
AgnesWangechi <engwangechi@gmail.com>
2023-12-12 21:06:13 +0300

    Create Hero section

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
 import Navbar from './Components/Navbar';
+import Hero from './Components/Hero/Hero';
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Navbar/>
+        <Hero/>
       
       </div>
     );
