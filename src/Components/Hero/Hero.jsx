bebde83088039d554d7e4e4716927ad65e456ff0
AgnesWangechi <engwangechi@gmail.com>
2023-12-12 23:21:41 +0300

    Hero underline

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -7,7 +7,7 @@ const Hero = () => {
         <section>
            <h5 id="H5">Hello, I am</h5> 
            <h2 id="H2">Agnes Wangechi,</h2>
-           <h3>A passionate Engineer.</h3>
+           <h3>A passionate Engi<span id="underline">neer.</span></h3>
             <p>I'm currently a Billing Engineer based in Nairobi, building products in the telecommunications space encompassing Voice, SMS, and Data products.
             </p>
         </section>
