2fc8a309c6f19155df935bf51d31ddf6354bb767
AgnesWangechi <engwangechi@gmail.com>
2023-12-12 21:37:59 +0300

    Hero h2

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -7,7 +7,7 @@ const Hero = () => {
         <section>
            <h5>Hello, I am</h5> 
            <h2>Agnes Wangechi,</h2>
-           <h2>A passionate Engineer.</h2>
+           <h3>A passionate Engineer.</h3>
             <p>I'm currently a Billing Engineer based in Nairobi, building products being</p>
         </section>
     </div>
