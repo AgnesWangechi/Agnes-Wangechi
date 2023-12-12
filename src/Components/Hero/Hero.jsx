5cfc6bebb0b412e0d02e5fc20e36d81a07365fb0
AgnesWangechi <engwangechi@gmail.com>
2023-12-12 21:23:14 +0300

    Hero contents

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -3,7 +3,16 @@ import "./Hero.css"
 
 const Hero = () => {
   return (
-    <div>Hero</div>
+    <div>
+        <section>
+           <h5>Hello, I am</h5> 
+           <h2>Agnes Wangechi.</h2>
+           <h2>A passionate Engineer</h2>
+        </section>
+        <section>
+            <p>I'm currently a Billing Engineer based in Nairobi, building products being</p>
+        </section>
+    </div>
   )
 }
 
