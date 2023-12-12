59a9dde28cdcfe9df469b2c611d3899218223615
AgnesWangechi <engwangechi@gmail.com>
2023-12-12 21:52:18 +0300

    Hero p tag

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -5,10 +5,11 @@ const Hero = () => {
   return (
     <div>
         <section>
-           <h5>Hello, I am</h5> 
-           <h2>Agnes Wangechi,</h2>
+           <h5 id="H5">Hello, I am</h5> 
+           <h2 id="H2">Agnes Wangechi,</h2>
            <h3>A passionate Engineer.</h3>
-            <p>I'm currently a Billing Engineer based in Nairobi, building products being</p>
+            <p>I'm currently a Billing Engineer based in Nairobi, building products in the telecommunications space encompassing being Voice, SMS, and data products.
+</p>
         </section>
     </div>
   )
