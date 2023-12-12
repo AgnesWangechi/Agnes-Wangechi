b3035c49f7664822a6148b0b1d31d25ff261de37
AgnesWangechi <engwangechi@gmail.com>
2023-12-12 18:31:19 +0300

    Navbar

---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div>
+        <nav>
+            
+            <div>
+                <ul>
+                    <li><a href="index.html">AboutMe</a></li>
+                    <li><a href="index.html">Experience</a></li>
+                    <li><a href="index.html">Projects</a></li>
+                    <li><a href="index.html">Contact</a></li>
+                    <li><a href="index.html">Resume</a></li>
+                </ul>
+            </div>
+        </nav>
+    </div>
+  )
+}
+
+export default Navbar
